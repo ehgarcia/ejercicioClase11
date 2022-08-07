ddef623f8f133fde1121b742a7f418c3ca5d0ce9
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2022-08-07 17:47:35 -0300

    probando cositas

---
+++ module/module.js
@@ -0,0 +1,13 @@
+const views = {
+    babbage: '/views/babbage.html',
+    bernersLee: '/views/berners-lee.html',
+    clarke: '/views/clarke.html',
+    hamilton: '/views/hamilton.html',
+    hopper: '/views/hopper.html',
+    index: '/views/index.html',
+    lovelace: '/views/lovelace.html',
+    turing: '/views/turing.html',
+    error: '/views/404.html'
+}
+
+module.exports = views;
